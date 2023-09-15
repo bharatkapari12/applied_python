d1935243f97894eb479e1c70074562229b59833b
Bharat Kumar Kapari <87240803+bharatkapari12@users.noreply.github.com>
2023-09-15 16:54:50 +0545

    Add files via upload

--- temperature_check.py
+++ temperature_check.py
@@ -16,6 +16,12 @@ class TemperatureTracker:
             temperature = int(input(f"Temperature day [{day}]: "))
             self.temperatures.append(temperature)
 
+        # Average Calculation
+    def calculateTemperature(self):
+        total_temperature = sum(self.temperatures)
+        average_temperature = (total_temperature / self.num_days)
+        return average_temperature
+
     def category_check(self, temperature):
         if temperature >= 85:
             return 'very hot'
@@ -24,17 +30,16 @@ class TemperatureTracker:
         else:
             return 'very cold'
 
-    def generate_report(self):
+    def displayInformation(self):
         print("\nDaily Temperature Report")
         for day, temperature in enumerate(self.temperatures, start=1):
             category = self.category_check(temperature)
             print(
                 f"Temperature day [{day}] = {temperature} Celsius ({category})")
             self.temperature_categories[category] += 1
-        
-        # Average Calculation
-        average_temperature = sum(self.temperatures) / self.num_days
-        print(f"The average temperature for {self.num_days} days = {average_temperature:.2f} Celsius")
+
+    def finalDisplay(self):
+        print(f"The average temperature for {self.num_days} days = {self.calculateTemperature()} Celsius")
         print(f"Number of very hot days: {self.temperature_categories['very hot']} day/s")
         print(f"Number of pleasant days: {self.temperature_categories['pleasant day']} day/s")
         print(f"Number of very cold days: {self.temperature_categories['very cold']} day/s")
@@ -44,10 +49,72 @@ def main():
     tt = TemperatureTracker()
     tt.initial_display()
     tt.input_data()
-    tt.generate_report()
+    tt.displayInformation()
+    tt.finalDisplay()
 
 
 main()
+
+
+
+
+
+
+
+
+# class TemperatureTracker:
+#     def __init__(self):
+#         self.company_name = ''
+#         self.num_days = 0
+#         self.temperatures = []
+#         self.temperature_categories = {
+#             'very hot': 0, 'pleasant day': 0, 'very cold': 0}
+
+#     def initial_display(self):
+#         self.company_name = input("Enter the company name: ")
+#         print(f"{self.company_name}\n----------Temperature Report-----------\n")
+
+#     def input_data(self):
+#         self.num_days = int(input("How many days to record: "))
+#         for day in range(1, self.num_days + 1):
+#             temperature = int(input(f"Temperature day [{day}]: "))
+#             self.temperatures.append(temperature)
+
+#     def category_check(self, temperature):
+#         if temperature >= 85:
+#             return 'very hot'
+#         elif temperature < 85 and temperature >= 60:
+#             return 'pleasant day'
+#         else:
+#             return 'very cold'
+
+#     def generate_report(self):
+#         print("\nDaily Temperature Report")
+#         for day, temperature in enumerate(self.temperatures, start=1):
+#             category = self.category_check(temperature)
+#             print(
+#                 f"Temperature day [{day}] = {temperature} Celsius ({category})")
+#             self.temperature_categories[category] += 1
+        
+#         # Average Calculation
+#         average_temperature = sum(self.temperatures) / self.num_days
+#         print(f"The average temperature for {self.num_days} days = {average_temperature:.2f} Celsius")
+#         print(f"Number of very hot days: {self.temperature_categories['very hot']} day/s")
+#         print(f"Number of pleasant days: {self.temperature_categories['pleasant day']} day/s")
+#         print(f"Number of very cold days: {self.temperature_categories['very cold']} day/s")
+
+
+# def main():
+#     tt = TemperatureTracker()
+#     tt.initial_display()
+#     tt.input_data()
+#     tt.generate_report()
+
+
+# main()
+
+
+
 
 
 # class TemperatureTracker:
